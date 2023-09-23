cdefe3d886523f27ed372b1b78367dc0f75c0ed7
João V. Pereira <JVPG789@hotmail.com>
2023-09-23 15:42:37 -0400

    Adição no ep[id] com o router

--- src/pages/Episodio/[id].jsx
+++ src/pages/Episodio/[id].jsx
@@ -1,22 +1,25 @@
 import { useRouter } from "next/router";
 import episodios from "@/utils/episodios";
 
-export default function Page(){
-    const router = useRouter();
-    const episodio = episodios[router.query.id-1]
+export default function Page() {
+  const router = useRouter();
+  const episodio = episodios[router.query.id - 1];
 
-    return(
-        <>
-      <h1>Pagina {router.query.id}</h1>
+  if (router.query.id > episodios.length) {
+    return <h1>Erro, esta pagina não existe</h1>;
+  } else {
+    return (
+      <>
+        <h1>Pagina {router.query.id}</h1>
 
-      <h2>{episodio.nome}</h2>
-      <p>Alunos: {episodio.alunos}</p>
-      <p>Turma: {episodio.turma}</p>
-      <p>
-        Descricao: <br />
-        {episodio.descricao}
-      </p>
-    </>
-
-    )
+        <h2>{episodio?.nome}</h2>
+        <p>Alunos: {episodio?.alunos}</p>
+        <p>Turma: {episodio?.turma}</p>
+        <p>
+          Descricao: <br />
+          {episodio?.descricao}
+        </p>
+      </>
+    );
+  }
 }
