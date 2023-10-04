576281c7151fb6c0571b56b85bf11e0b69445c5e
João V. Pereira <JVPG789@hotmail.com>
2023-10-04 14:27:19 -0400

    Correção de um bug

--- src/pages/Episodio/[id].jsx
+++ src/pages/Episodio/[id].jsx
@@ -4,12 +4,19 @@ import returnSrc from "@/utils/returnSrc";
 import styles from "@/styles/Episodio.module.scss";
 import { useState } from "react";
 
+export async function getServerSideProps(context) {
+  return {
+      props: {},
+  };
+}
+
 export default function Page() {
   const router = useRouter();
   const episodio = episodios[router.query.id - 1];
   const src = returnSrc(episodio.audioSrc);
   const [podeCarregar, setPodeCarregar] = useState(false);
 
+  
   if (router.query.id > episodios.length) {
     return <h1>Erro, esta pagina não existe</h1>;
   } else {
