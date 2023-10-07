0f07c2a611224dda0d36bede99fc99fa987f8d79
João V. Pereira <JVPG789@hotmail.com>
2023-10-07 19:56:27 -0400

    Desenvolvendo searchResults

--- src/components/CardList/Card/index.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "@/components/CardList/Card/styles.module.scss";
-import Link from "next/link";
-
-export default function Card () {
-    return (
-        <>
-        </>
-    )
-}
--- src/components/SearchResults/index.jsx
+++ src/components/SearchResults/index.jsx
@@ -1,48 +1,112 @@
 import Link from "next/link";
-import styles from "./SearchResults.module.scss"
+import styles from "./SearchResults.module.scss";
 import episodios from "@/utils/episodios";
+import { useState } from "react";
+import EpisodioButton from "../EpisodioButton";
 
+export default function SearchResults({ searchValue, ...props }) {
+  const [haveNmResults, setHaveNmResults] = useState(false);
+  const [haveAlResults, setHaveAlResults] = useState(false);
+  const [haveCapResults, setHaveCapResults] = useState(false);
 
-export default function SearchResults({searchValue,...props}){
+  return (
+    <>
+      {/*resultado em nome de episodio*/}
 
-    
-    
-    return(
-        <>
-        {/*resultado em nome de episodio*/}
+      <div className={styles.results}>
+        {haveNmResults && <h3>Por número do episódio:</h3>}
 
-        <h3>Por nome:</h3>
-        <ul>
-            <div>
-                {episodios.map((episodio, index)=>{
-                if(episodio.nome.toUpperCase().includes(searchValue.toUpperCase())) return(
-                    <li key={episodio.id}> <Link href={`/Episodio/${episodio.id}`}>{episodio.nome}</Link></li>
-                )
-                })}
-            </div>
-        </ul>
+        <div>
+          {episodios.map((episodio, index) => {
+            if (
+              episodio.nome.toUpperCase().includes(searchValue.toUpperCase())
+            ) {
+              if (!haveNmResults) setHaveNmResults(true);
+              return (
+                <div
+                  key={episodio.nome}
+                  className={styles.episodioButton_container}
+                >
+                  <Link
+                    className={styles.linkEpButton}
+                    href={`/Episodio/${episodio.id}`}
+                  >
+                    <EpisodioButton
+                      nome={episodio.nome}
+                      capitulos={episodio.capitulos}
+                      alunos={episodio.alunos}
+                    />
+                  </Link>
+                </div>
+              );
+            }
+          })}
+        </div>
+      </div>
 
-        <h3>Por alunos:</h3>
-        <ul>
-            <div>
-                {episodios.map((episodio, index)=>{
-                if(episodio.alunos.toUpperCase().includes(searchValue.toUpperCase())) return(
-                    <li key={episodio.id}> <Link href={`/Episodio/${episodio.id}`}>{episodio.nome}</Link></li>
-                )
-                })}
-            </div>
-        </ul>
+      <div className={styles.results}>
+        {haveAlResults && <h3>Por nome dos alunos:</h3>}
 
-        <h3>Por capitulos:</h3>
-        <ul>
-            <div>
-                {episodios.map((episodio, index)=>{
-                if(episodio.capitulos.toUpperCase().includes(searchValue.toUpperCase())) return(
-                    <li key={episodio.id}> <Link href={`/Episodio/${episodio.id}`}>{episodio.nome}</Link></li>
-                )
-                })}
-            </div>
-        </ul>
-        </>
-    )
+        <div>
+          {episodios.map((episodio, index) => {
+            if (
+              episodio.alunos.toUpperCase().includes(searchValue.toUpperCase())
+            ) {
+              if (!haveAlResults) setHaveAlResults(true);
+              return (
+                <div
+                  key={episodio.nome}
+                  className={styles.episodioButton_container}
+                >
+                  <Link
+                    className={styles.linkEpButton}
+                    href={`/Episodio/${episodio.id}`}
+                  >
+                    <EpisodioButton
+                      nome={episodio.nome}
+                      capitulos={episodio.capitulos}
+                      alunos={episodio.alunos}
+                    />
+                  </Link>
+                </div>
+              );
+            }
+          })}
+        </div>
+      </div>
+
+      <div className={styles.results}>
+        {haveCapResults && <h3>Por número dos capítulos:</h3>}
+
+        <div>
+          {episodios.map((episodio, index) => {
+            if (
+              episodio.capitulos
+                .toUpperCase()
+                .includes(searchValue.toUpperCase())
+            ) {
+              if (!haveCapResults) setHaveCapResults(true);
+              return (
+                <div
+                  key={episodio.nome}
+                  className={styles.episodioButton_container}
+                >
+                  <Link
+                    className={styles.linkEpButton}
+                    href={`/Episodio/${episodio.id}`}
+                  >
+                    <EpisodioButton
+                      nome={episodio.nome}
+                      capitulos={episodio.capitulos}
+                      alunos={episodio.alunos}
+                    />
+                  </Link>
+                </div>
+              );
+            }
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
