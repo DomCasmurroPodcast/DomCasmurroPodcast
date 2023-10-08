81463b576fb84794803316543ae297a80cadff7c
João V. Pereira <JVPG789@hotmail.com>
2023-10-07 21:47:06 -0400

    Desenvolvido versão mobile

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,11 +4,16 @@ import episodios from "@/utils/episodios";
 import Link from "next/link";
 import SearchBar from "@/components/SearchBar";
 import SearchResults from "@/components/SearchResults";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Image from "next/image";
 
 export default function Home() {
+  const ref = useRef(null)
   const [searchValue, setSearchValue] = useState("");
+
+  const handleClick = () => {
+    ref.current?.scrollIntoView({ behavior: 'smooth' });
+  };
 
   return (
     <>
@@ -19,10 +24,10 @@ export default function Home() {
             <br />
             Podcast
           </h1>
-          <div className={styles.scroll}></div>
+          <div className={styles.scroll} onClick={handleClick}></div>
         </div>
 
-        <div className={styles.mainContent}>
+        <div ref={ref} className={styles.mainContent}>
           <SearchBar
             className={styles.searchBarHome}
             exportValue={setSearchValue}
@@ -64,7 +69,7 @@ export default function Home() {
 
         <div className={styles.footer}>
           <p>Sed ullamcorper nunc in viverra tempus. Ut consectetur libero ac ipsum efficitur, sed bibendum nisi tincidunt. Quisque pretium elit quis nisi consectetur faucibus. Duis ac purus non risus faucibus maximus. Proin egestas, sapien eu hendrerit hendrerit, erat magna bibendum eros, vitae scelerisque erat turpis quis metus. Quisque in metus dui. Ut at efficitur tellus. Nulla malesuada ante diam, eu suscipit mauris consectetur sit amet. Aenean neque est, accumsan non elementum vel, blandit et purus. Duis eu vehicula mi, a dictum sem. Etiam mauris velit, eleifend vitae porttitor eu, sagittis sit amet sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec vel pharetra nibh. Donec ac accumsan tortor. Sed ut suscipit augue, ac tincidunt quam. Vivamus vitae dui interdum, mattis eros vitae, euismod urna.</p>
-          <Link className={styles.gitHub_link} href="https://github.com/DomCasmurroPodcast/DomCasmurroPodcast.github.io">
+          <Link title="Veja código da página no GitHub" className={styles.gitHub_link} href="https://github.com/DomCasmurroPodcast/DomCasmurroPodcast.github.io">
             <Image className={styles.gitHub_image} src="/imgs/Github_Logo.svg" width={100} height={100} alt="Veja código no GitHub"/>
           </Link>
         </div>
