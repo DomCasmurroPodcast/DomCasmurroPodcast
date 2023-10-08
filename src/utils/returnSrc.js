6c00b6be441d965f13acd6cc86d296fea5104373
João V. Pereira <JVPG789@hotmail.com>
2023-10-07 22:11:18 -0400

    Configurando 2

--- src/utils/returnSrc.js
+++ src/utils/returnSrc.js
@@ -1,4 +1,5 @@
 export default function returnSrc(audioSrc){
+    if(audioSrc == null) return null;
     const src = "https://drive.google.com/uc?export=open&id=" + audioSrc.split("/")[5]
     return src;
 }
