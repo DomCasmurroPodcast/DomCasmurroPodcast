58aecc44363867b25212057d7e555c14b2004a71
João V. Pereira <JVPG789@hotmail.com>
2023-10-07 21:29:36 -0400

    Adição do footer temporario

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import SearchBar from "@/components/SearchBar";
 import SearchResults from "@/components/SearchResults";
 import { useState } from "react";
-
+import Image from "next/image";
 
 export default function Home() {
   const [searchValue, setSearchValue] = useState("");
@@ -14,43 +14,60 @@ export default function Home() {
     <>
       <div className={styles.bodyContainer}>
         <div className={styles.firstView}>
-          <h1>Dom Casmurro<br/>Podcast</h1>
+          <h1>
+            Dom Casmurro
+            <br />
+            Podcast
+          </h1>
           <div className={styles.scroll}></div>
         </div>
-      </div>
 
-      <div className={styles.mainContent}>
-        
-        
-        <SearchBar className={styles.searchBarHome} exportValue={setSearchValue}/>
-        
-        <div className={styles.episodios}>
+        <div className={styles.mainContent}>
+          <SearchBar
+            className={styles.searchBarHome}
+            exportValue={setSearchValue}
+          />
 
-          {searchValue == ""  && <div className={styles.todosEpisodios}>
-            <h3>Todos Episódios:</h3>
-            <div className={styles.todosEpisodios_list}>
-              {episodios.map((episodio, index)=>(
-                <div key={episodio.nome} className={styles.episodioButton_container}>
-                  <Link className={styles.linkEpButton} href={`/Episodio/${episodio.id}`}>
-                    <EpisodioButton 
-                      nome={episodio.nome}
-                      capitulos={episodio.capitulos}
-                      alunos={episodio.alunos}
-                      />
-                  </Link>
+          <div className={styles.episodios}>
+            {searchValue == "" && (
+              <div className={styles.todosEpisodios}>
+                <h3>Todos Episódios:</h3>
+                <div className={styles.todosEpisodios_list}>
+                  {episodios.map((episodio, index) => (
+                    <div
+                      key={episodio.nome}
+                      className={styles.episodioButton_container}
+                    >
+                      <Link
+                        className={styles.linkEpButton}
+                        href={`/Episodio/${episodio.id}`}
+                      >
+                        <EpisodioButton
+                          nome={episodio.nome}
+                          capitulos={episodio.capitulos}
+                          alunos={episodio.alunos}
+                        />
+                      </Link>
+                    </div>
+                  ))}
                 </div>
-              ))}
-            </div>
-          </div>}
+              </div>
+            )}
 
-          {searchValue != "" &&
-          <div className={styles.resultadosPesquisa}>
-            <SearchResults searchValue={searchValue}/>
+            {searchValue != "" && (
+              <div className={styles.resultadosPesquisa}>
+                <SearchResults searchValue={searchValue} />
+              </div>
+            )}
           </div>
-          }
-
         </div>
 
+        <div className={styles.footer}>
+          <p>Sed ullamcorper nunc in viverra tempus. Ut consectetur libero ac ipsum efficitur, sed bibendum nisi tincidunt. Quisque pretium elit quis nisi consectetur faucibus. Duis ac purus non risus faucibus maximus. Proin egestas, sapien eu hendrerit hendrerit, erat magna bibendum eros, vitae scelerisque erat turpis quis metus. Quisque in metus dui. Ut at efficitur tellus. Nulla malesuada ante diam, eu suscipit mauris consectetur sit amet. Aenean neque est, accumsan non elementum vel, blandit et purus. Duis eu vehicula mi, a dictum sem. Etiam mauris velit, eleifend vitae porttitor eu, sagittis sit amet sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec vel pharetra nibh. Donec ac accumsan tortor. Sed ut suscipit augue, ac tincidunt quam. Vivamus vitae dui interdum, mattis eros vitae, euismod urna.</p>
+          <Link className={styles.gitHub_link} href="https://github.com/DomCasmurroPodcast/DomCasmurroPodcast.github.io">
+            <Image className={styles.gitHub_image} src="/imgs/Github_Logo.svg" width={100} height={100} alt="Veja código no GitHub"/>
+          </Link>
+        </div>
       </div>
     </>
   );
