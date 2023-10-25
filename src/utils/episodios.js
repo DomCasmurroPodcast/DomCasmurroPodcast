448a747d65a101f77420181134089077d4a90829
João V. Pereira <JVPG789@hotmail.com>
2023-10-25 14:49:50 -0400

    Atualização final 25/10

--- src/utils/episodios.js
+++ src/utils/episodios.js
@@ -11,102 +11,102 @@ const Episódios = [
   {
     id: 2,
     nome: "Episódio 2",
-    audioSrc: "/",
+    audioSrc: "https://drive.google.com/file/d/18dYO7p36LP9gTwN98Uyzuvon2dQfyvZF/view?usp=drive_link",
     capitulos: "6, 7, 8, 9, 10, 11",
     alunos: "Ana Carolina, Vitória Gonçalvez, Wanda Pereira",
-    descricao: ".",
+    descricao: "Nosso capítulo vai falar sobre o tio Cosme, Dona Glória e sobre a promessa que o livro em si fala.",
     turma: "2ºB"
   },
   {
     id: 3,
     nome: "Episódio 3",
-    audioSrc: "/",
+    audioSrc: "https://drive.google.com/file/d/1s3YcDrWNJR8HCkmOV2bmvQr8MUKYPtF_/view?usp=drive_link",
     capitulos: "12, 13, 14, 15, 16, 17",
     alunos: "Emylly Samylly, Laís Amaro, João Phelipe",
-    descricao: ".",
+    descricao: "No terceiro episódio do podcast, os alunos Emylly Samylly, João Phelipe e Laís Amaro irão apresentar os acontecimentos dos capítulos 12 a 17 e continuar a narração da história enviada pelo seguidor Dom Casmurro.",
     turma: "2ºA"
   },
   {
     id: 4,
     nome: "Episódio 4",
-    audioSrc: "/",
+    audioSrc: "https://drive.google.com/file/d/1u2MpgXmndIvg9QvMfFucNOUCe6cpv6GK/view?usp=drive_link",
     capitulos: "18, 19, 20, 21, 22",
     alunos: "Patrícia O. Silva, Leonardo Pereira",
-    descricao: ".",
+    descricao: "Bentinho, apesar de suas dificuldades no seminário, explora uma amizade cada vez mais forte com Escobar, se tornando um dos principais focos em sua vida. Entretanto, Bentinho deixa o seminário após uma doença, o que ele não sabia é que encontraria novamente a Capitu, sua amiga de infância. Mas o que acontece ao longo desses capítulos? É o que veremos no episódio de hoje.",
     turma: "2ºA"
   },
   {
     id: 5,
     nome: "Episódio 5",
-    audioSrc: "/",
+    audioSrc: "https://drive.google.com/file/d/14fCxEAfAy9Btlw1MEQ33i2FKMGN8-bi5/view?usp=drive_link",
     capitulos: "23, 24, 25, 26, 27",
     alunos: "Anna Clara, Andressa",
-    descricao: ".",
+    descricao: "No 5° episódio apresentado por Andressa Alypio e Anna Clara sobre os capítulos 23 á 27 e iremos contar algumas fofocas feitas durante o passeio público.",
     turma: "2ºA",
     
   },
   {
     id: 6,
     nome: "Episódio 6",
-    audioSrc: "/",
+    audioSrc: "https://drive.google.com/file/d/1VtMvmGSkU5L86WD0GdNkxZSt6CdltYc9/view?usp=drive_link",
     capitulos: "28, 29, 30, 31, 32",
     alunos: "Keila R. Silva, Caliane Antonielly",
-    descricao: ".",
+    descricao: "Bentinho tenta encontrar uma forma pra desistir do seminário e ficar com sua amada capitu.",
     turma: "2ºB"
   },
   {
     id: 7,
     nome: "Episódio 7",
-    audioSrc: "/",
+    audioSrc: "https://drive.google.com/file/d/1-vTp73I-GCpm8h326nWjcJ1rjeIhnLDy/view?usp=drive_link",
     capitulos: "33, 34, 35, 36, 37",
     alunos: "Beatriz Oliveira, Dáfeli",
-    descricao: ".",
+    descricao: "O 7° episódio do podCast foi apresentado pelas alunas Beatriz Rocha Oliveira e Dáfelli Kauani Bonazzi, o qual consiste em referências os capítulos 33 á 37.",
     turma: "2ºA"
   },
   {
     id: 8,
     nome: "Episódio 8",
-    audioSrc: "/",
+    audioSrc: "https://drive.google.com/file/d/1eV729OKt0YSGwsxm8DWjT5CSxFv1j4In/view?usp=drive_link",
     capitulos: "38, 39, 40, 41, 42",
     alunos: "Uédina Clarice, Nicole Eduarda",
-    descricao: ".",
+    descricao: "Bentinho confuso entre ficar com a Capitu ou seguir com a promessa de sua mãe e se tornar padre…",
     turma: "2ºB"
   },
   {
     id: 9,
     nome: "Episódio 9",
-    audioSrc: "/",
+    audioSrc: "https://drive.google.com/file/d/1HveiHHkWY_RERz0TEJEKVwY6vAG5apnu/view?usp=drive_link",
     capitulos: "43, 44, 45, 46, 47, 48",
     alunos: "Anne Luize, Marcos, Karoline",
-    descricao: ".",
+    descricao: "O Podcast segue a história, relacionando o romance de Bentinho e sua dúvida entre seguir seu caminho na igreja ou ir atrás do amor da sua vida, mas acaba passando por dificuldades ao preferir seguir com sua vida na igreja, e um tempo depois descobre que sua amada estava com o seu melhor amigo e se perdeu na sua vida após esse choque de realidade, e procurou um senhor misterioso que acabou não ajudando muito a resolver seus problemas …",
     turma: "2ºA",
     
   },
   {
     id: 10,
     nome: "Episódio 10",
-    audioSrc: "/",
+    audioSrc: "https://drive.google.com/file/d/1LQuFk6G_-ciknKZRkq3ZVeCzg4EFmWyx/view?usp=drive_link",
     capitulos: "49, 50, 51, 52, 53",
     alunos: "Fernanda Gonçalves, Luan",
-    descricao: ".",
+    descricao: "O 10° episódio do Podcast foi apresentado pelos alunos Fernanda Gonçalves da Costa e Kewrison Luan Caminha Rodrigues, da turma 2° B, Informática. Os capítulos apresentados foram os da sessão 49-53.",
     turma: "2ºB"
   },
   {
     id: 11,
     nome: "Episódio 11",
-    audioSrc: "/",
+    audioSrc: "https://drive.google.com/file/d/1W2A65XTS6uX3ZOzC1NFH2-nBOM4wG97J/view?usp=drive_link",
     capitulos: "54, 55, 56, 57, 58",
     alunos: "Diego, João Pedro, Uéliton",
-    descricao: ".",
+    descricao: "Esse é o 11° episódio e foi apresentado por Uéliton, João Pedro e Diego, os capítulos apresentados foram do 54 ao 58 onde continuamos a história de dom Casmurro.",
     turma: "2ºA"
   },
   {
     id: 12,
     nome: "Episódio 12",
-    audioSrc: "/",
+    audioSrc: "https://drive.google.com/file/d/1ATuMDDXtOCNhiwogz6aMQKH7-PveEmh3/view?usp=drive_link",
     capitulos: "59, 60, 61, 62, 63, 64",
     alunos: "Elias Moreira, Natam Meneght",
-    descricao: ".",
+    descricao: "Em um suspense envolvente, Dom Casmurro narra mais segredos intrigantes sobre seu amigo Bentinho, revelando dúvidas inquietantes que assombram a vida de seu amigo",
     turma: "2ºB"
   },
   {
@@ -131,70 +131,16 @@ const Episódios = [
   {
     id: 15,
     nome: "Episódio 15",
-    audioSrc: "/",
+    audioSrc: "https://drive.google.com/file/d/1gB70FUd9GONa5Hh20z3xxyiWpHMTTnO7/view?usp=drive_link",
     capitulos: "77, 78, 79, 80, 81, 82",
     alunos: "Edmara, Suellen",
-    descricao: ".",
+    descricao: "Nesse podcast falamos um pouco sobre alguns acontecimentos dos capítulos 77 a 82. Fizemos com que os personagens fossem famosos por terem feito um filme, e que algo pessoal que estivesse acontecendo na vida deles virasse fofoca para seus “fãs”, entretendo-os e ao mesmo tempo dando um resumos dos capítulos.",
     turma: "2ºA"
   },
   {
     id: 16,
     nome: "Episódio 16",
-    audioSrc: "/",
-    capitulos: "83, 84, 85, 86, 87, 88",
-    alunos: "Sidnei Salustiano, Pedro Baltazar, Erick Brayan",
-    descricao: ".",
-    turma: "2ºB"
-  },
-  {
-    id: 16,
-    nome: "Episódio 15",
-    audioSrc: "/",
-    capitulos: "77, 78, 79, 80, 81, 82",
-    alunos: "Edmara, Suellen",
-    descricao: ".",
-    turma: "2ºA"
-  },
-  {
-    id: 17,
-    nome: "Episódio 16",
-    audioSrc: "/",
-    capitulos: "83, 84, 85, 86, 87, 88",
-    alunos: "Sidnei Salustiano, Pedro Baltazar, Erick Brayan",
-    descricao: ".",
-    turma: "2ºB"
-  },
-  {
-    id: 15,
-    nome: "Episódio 15",
-    audioSrc: "/",
-    capitulos: "77, 78, 79, 80, 81, 82",
-    alunos: "Edmara, Suellen",
-    descricao: ".",
-    turma: "2ºA"
-  },
-  {
-    id: 16,
-    nome: "Episódio 16",
-    audioSrc: "/",
-    capitulos: "83, 84, 85, 86, 87, 88",
-    alunos: "Sidnei Salustiano, Pedro Baltazar, Erick Brayan",
-    descricao: ".",
-    turma: "2ºB"
-  },
-  {
-    id: 15,
-    nome: "Episódio 15",
-    audioSrc: "/",
-    capitulos: "77, 78, 79, 80, 81, 82",
-    alunos: "Edmara, Suellen",
-    descricao: ".",
-    turma: "2ºA"
-  },
-  {
-    id: 16,
-    nome: "Episódio 16",
-    audioSrc: "/",
+    audioSrc: "https://drive.google.com/file/d/1DUjzJp0dK4DkDGtVvEpGLr0Sf70AsYCB/view?usp=drive_link",
     capitulos: "83, 84, 85, 86, 87, 88",
     alunos: "Sidnei Salustiano, Pedro Baltazar, Erick Brayan",
     descricao: ".",
@@ -230,10 +176,10 @@ const Episódios = [
   {
     id: 20,
     nome: "Episódio 20",
-    audioSrc: "/",
+    audioSrc: "https://drive.google.com/file/d/125lhM25n-7DjZNj1_zWKZZ1UcdhhSx3y/view?usp=drive_link",
     capitulos: "107, 108, 109, 110, 111, 112",
     alunos: "Eduardo Fernandes, Mateus Orocondo, Yan Fernando",
-    descricao: ".",
+    descricao: "(Sem descrição)",
     turma: "2ºB"
   },
   {
@@ -248,10 +194,10 @@ const Episódios = [
   {
     id: 22,
     nome: "Episódio 22",
-    audioSrc: "/",
+    audioSrc: "https://drive.google.com/file/d/1uwjN-c14o4H4VNhRsyJVDGFrZXYd3Cdh/view?usp=drive_link",
     capitulos: "119, 120, 121, 122, 123, 124",
     alunos: "Pedro Noemerg, Aline Noemerg",
-    descricao: ".",
+    descricao: "Junte-se a nós para uma análise profunda e emocionante da trágica morte de Escobar, uma história repleta de mistério e reviravoltas, neste podcast que desvenda os segredos por trás de um destino surpreendente.",
     turma: "2ºB"
   },
   {
@@ -266,28 +212,28 @@ const Episódios = [
   {
     id: 24,
     nome: "Episódio 24",
-    audioSrc: "/",
+    audioSrc: "https://drive.google.com/file/d/10PMKgCT1KDU_EdAYeVKLEOKSVgOXMzrt/view?usp=drive_link",
     capitulos: "131, 132, 133, 134, 135, 136",
     alunos: "Emanuel, Henrique",
-    descricao: ".",
+    descricao: "Este episódio foi apresentado pelos alunos Emanuel e Henrique, continua com o próximo trecho da história do nosso seguidor Dom Casmurro, e explora um dos momentos mais intrigantes de sua vida.",
     turma: "2ºA"
   },
   {
     id: 25,
     nome: "Episódio 25",
-    audioSrc: "/",
+    audioSrc: "https://drive.google.com/file/d/1u_W6oUwloztChdAb8-c0qAsKt0MG9cpO/view?usp=drive_link",
     capitulos: "137, 138, 139, 140, 141, 142",
     alunos: "Gabriely, Tamires",
-    descricao: ".",
+    descricao: "Bom, a nossa parte fala sobre as dificuldades do casamento de Capitu e também a desconfiança de Benito sobre a sua esposa estar te traindo.",
     turma: "2ºA"
   },
   {
     id: 26,
     nome: "Episódio 26",
-    audioSrc: "/",
+    audioSrc: "https://drive.google.com/file/d/1MFvCLY-TAvnOatS5o6RCr2LEzzZ1GFMX/view?usp=drive_link",
     capitulos: "143, 144, 145, 146, 147, 148",
-    alunos: "Kauã, Leonardo Neiva",
-    descricao: ".",
+    alunos: "Tarcísio, Kauã, Leonardo Neiva",
+    descricao: "No último episódio do podcast, os alunos Leonardo e Tarcísio  irão apresentar os acontecimentos dos capítulos 143 a 148 e continuar a narração da história enviada pelo seguidor Dom Casmurro.",
     turma: "2ºA"
   },
 ];
