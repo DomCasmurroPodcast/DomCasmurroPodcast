ecc1100f20082d93e86cbff3df8ba18ad47edf91
João V. Pereira <JVPG789@hotmail.com>
2023-10-25 14:15:18 -0400

    Atualização dos episodios

--- src/utils/episodios.js
+++ src/utils/episodios.js
@@ -1,152 +1,295 @@
-const episodios = [
+const Episódios = [
   {
     id: 1,
-    nome: "Episodio 1",
+    nome: "Episódio 1",
     audioSrc: "https://drive.google.com/file/d/11lINxK011UrKpjjq8a0qelNuU1U-30Mu/view?usp=sharing",
     capitulos: "1, 2, 3, 4, 5",
     alunos: "Amanda, Bruna",
     descricao: "O 1° episódio do Podcast foi apresentado pelas alunas Amanda e Bruna e consiste na apresentação do mesmo juntamente com uma breve introdução da história que será contada.",
-    turma: "2ºA Informática",
-    capitulos: "1, 2, 3, 4 e 5"
+    turma: "2ºA",
   },
   {
     id: 2,
-    nome: "Episodio 2",
+    nome: "Episódio 2",
     audioSrc: "/",
     capitulos: "6, 7, 8, 9, 10, 11",
     alunos: "Ana Carolina, Vitória Gonçalvez, Wanda Pereira",
-    descricao: "A maluca conteporanea é completamente sociedade...",
+    descricao: ".",
     turma: "2ºB"
   },
   {
     id: 3,
-    nome: "Episodio 3",
+    nome: "Episódio 3",
     audioSrc: "/",
     capitulos: "12, 13, 14, 15, 16, 17",
     alunos: "Emylly Samylly, Laís Amaro, João Phelipe",
-    descricao: "A sociedade conteporanea é completamente maluca...",
+    descricao: ".",
     turma: "2ºA"
   },
   {
     id: 4,
-    nome: "Episodio 4",
+    nome: "Episódio 4",
     audioSrc: "/",
     capitulos: "18, 19, 20, 21, 22",
     alunos: "Patrícia O. Silva, Leonardo Pereira",
-    descricao: "A maluca conteporanea é completamente sociedade...",
+    descricao: ".",
     turma: "2ºA"
   },
   {
     id: 5,
-    nome: "Episodio 5",
+    nome: "Episódio 5",
     audioSrc: "/",
-    capitulos: "1, 2, 3, 4, 5",
-    alunos: "Amanda, Bruna",
-    descricao: "A sociedade conteporanea é completamente maluca...",
-    turma: "2ºA Informática",
-    capitulos: "1, 2, 3, 4 e 5"
+    capitulos: "23, 24, 25, 26, 27",
+    alunos: "Anna Clara, Andressa",
+    descricao: ".",
+    turma: "2ºA",
+    
   },
   {
     id: 6,
-    nome: "Episodio 6",
+    nome: "Episódio 6",
     audioSrc: "/",
-    capitulos: "6, 7, 8, 9, 10, 11",
-    alunos: "Ana Carolina, Vitória Gonçalvez, Wanda Pereira",
-    descricao: "A maluca conteporanea é completamente sociedade...",
+    capitulos: "28, 29, 30, 31, 32",
+    alunos: "Keila R. Silva, Caliane Antonielly",
+    descricao: ".",
     turma: "2ºB"
   },
   {
     id: 7,
-    nome: "Episodio 7",
+    nome: "Episódio 7",
     audioSrc: "/",
-    capitulos: "12, 13, 14, 15, 16, 17",
-    alunos: "Emylly Samylly, Laís Amaro, João Phelipe",
-    descricao: "A sociedade conteporanea é completamente maluca...",
+    capitulos: "33, 34, 35, 36, 37",
+    alunos: "Beatriz Oliveira, Dáfeli",
+    descricao: ".",
     turma: "2ºA"
   },
   {
     id: 8,
-    nome: "Episodio 8",
+    nome: "Episódio 8",
     audioSrc: "/",
-    capitulos: "18, 19, 20, 21, 22",
-    alunos: "Patrícia O. Silva, Leonardo Pereira",
-    descricao: "A maluca conteporanea é completamente sociedade...",
-    turma: "2ºA"
+    capitulos: "38, 39, 40, 41, 42",
+    alunos: "Uédina Clarice, Nicole Eduarda",
+    descricao: ".",
+    turma: "2ºB"
   },
   {
     id: 9,
-    nome: "Episodio 9",
+    nome: "Episódio 9",
     audioSrc: "/",
-    capitulos: "1, 2, 3, 4, 5",
-    alunos: "Amanda, Bruna",
-    descricao: "A sociedade conteporanea é completamente maluca...",
-    turma: "2ºA Informática",
-    capitulos: "1, 2, 3, 4 e 5"
+    capitulos: "43, 44, 45, 46, 47, 48",
+    alunos: "Anne Luize, Marcos, Karoline",
+    descricao: ".",
+    turma: "2ºA",
+    
   },
   {
     id: 10,
-    nome: "Episodio 10",
+    nome: "Episódio 10",
     audioSrc: "/",
-    capitulos: "6, 7, 8, 9, 10, 11",
-    alunos: "Ana Carolina, Vitória Gonçalvez, Wanda Pereira",
-    descricao: "A maluca conteporanea é completamente sociedade...",
+    capitulos: "49, 50, 51, 52, 53",
+    alunos: "Fernanda Gonçalves, Luan",
+    descricao: ".",
     turma: "2ºB"
   },
   {
     id: 11,
-    nome: "Episodio 11",
+    nome: "Episódio 11",
     audioSrc: "/",
-    capitulos: "12, 13, 14, 15, 16, 17",
-    alunos: "Emylly Samylly, Laís Amaro, João Phelipe",
-    descricao: "A sociedade conteporanea é completamente maluca...",
+    capitulos: "54, 55, 56, 57, 58",
+    alunos: "Diego, João Pedro, Uéliton",
+    descricao: ".",
     turma: "2ºA"
   },
   {
     id: 12,
-    nome: "Episodio 12",
+    nome: "Episódio 12",
     audioSrc: "/",
-    capitulos: "18, 19, 20, 21, 22",
-    alunos: "Patrícia O. Silva, Leonardo Pereira",
-    descricao: "A maluca conteporanea é completamente sociedade...",
-    turma: "2ºA"
+    capitulos: "59, 60, 61, 62, 63, 64",
+    alunos: "Elias Moreira, Natam Meneght",
+    descricao: ".",
+    turma: "2ºB"
   },
   {
     id: 13,
-    nome: "Episodio 13",
+    nome: "Episódio 13",
     audioSrc: "/",
-    capitulos: "1, 2, 3, 4, 5",
-    alunos: "Amanda, Bruna",
-    descricao: "A sociedade conteporanea é completamente maluca...",
-    turma: "2ºA Informática",
-    capitulos: "1, 2, 3, 4 e 5"
+    capitulos: "65, 66, 67, 68, 69, 70",
+    alunos: "Amara, Gabriel",
+    descricao: ".",
+    turma: "2ºA ",
+    
   },
   {
     id: 14,
-    nome: "Episodio 14",
+    nome: "Episódio 14",
     audioSrc: "/",
-    capitulos: "6, 7, 8, 9, 10, 11",
-    alunos: "Ana Carolina, Vitória Gonçalvez, Wanda Pereira",
-    descricao: "A maluca conteporanea é completamente sociedade...",
+    capitulos: "71, 72, 73, 74, 75, 76",
+    alunos: "Alisson Dias, Thauan Wictor",
+    descricao: ".",
     turma: "2ºB"
   },
   {
     id: 15,
-    nome: "Episodio 15",
+    nome: "Episódio 15",
     audioSrc: "/",
-    capitulos: "12, 13, 14, 15, 16, 17",
-    alunos: "Emylly Samylly, Laís Amaro, João Phelipe",
-    descricao: "A sociedade conteporanea é completamente maluca...",
+    capitulos: "77, 78, 79, 80, 81, 82",
+    alunos: "Edmara, Suellen",
+    descricao: ".",
     turma: "2ºA"
   },
   {
     id: 16,
-    nome: "Episodio 16",
+    nome: "Episódio 16",
     audioSrc: "/",
-    capitulos: "18, 19, 20, 21, 22",
-    alunos: "Patrícia O. Silva, Leonardo Pereira",
-    descricao: "A maluca conteporanea é completamente sociedade...",
+    capitulos: "83, 84, 85, 86, 87, 88",
+    alunos: "Sidnei Salustiano, Pedro Baltazar, Erick Brayan",
+    descricao: ".",
+    turma: "2ºB"
+  },
+  {
+    id: 16,
+    nome: "Episódio 15",
+    audioSrc: "/",
+    capitulos: "77, 78, 79, 80, 81, 82",
+    alunos: "Edmara, Suellen",
+    descricao: ".",
+    turma: "2ºA"
+  },
+  {
+    id: 17,
+    nome: "Episódio 16",
+    audioSrc: "/",
+    capitulos: "83, 84, 85, 86, 87, 88",
+    alunos: "Sidnei Salustiano, Pedro Baltazar, Erick Brayan",
+    descricao: ".",
+    turma: "2ºB"
+  },
+  {
+    id: 15,
+    nome: "Episódio 15",
+    audioSrc: "/",
+    capitulos: "77, 78, 79, 80, 81, 82",
+    alunos: "Edmara, Suellen",
+    descricao: ".",
+    turma: "2ºA"
+  },
+  {
+    id: 16,
+    nome: "Episódio 16",
+    audioSrc: "/",
+    capitulos: "83, 84, 85, 86, 87, 88",
+    alunos: "Sidnei Salustiano, Pedro Baltazar, Erick Brayan",
+    descricao: ".",
+    turma: "2ºB"
+  },
+  {
+    id: 15,
+    nome: "Episódio 15",
+    audioSrc: "/",
+    capitulos: "77, 78, 79, 80, 81, 82",
+    alunos: "Edmara, Suellen",
+    descricao: ".",
+    turma: "2ºA"
+  },
+  {
+    id: 16,
+    nome: "Episódio 16",
+    audioSrc: "/",
+    capitulos: "83, 84, 85, 86, 87, 88",
+    alunos: "Sidnei Salustiano, Pedro Baltazar, Erick Brayan",
+    descricao: ".",
+    turma: "2ºB"
+  },
+  {
+    id: 17,
+    nome: "Episódio 17",
+    audioSrc: "/",
+    capitulos: "89, 90, 91, 92, 93, 94",
+    alunos: "Luiz Guilhermy, Adrielle",
+    descricao: ".",
+    turma: "2ºA"
+  },
+  {
+    id: 18,
+    nome: "Episódio 18",
+    audioSrc: "/",
+    capitulos: "95, 96, 97, 98, 99, 100",
+    alunos: "Janine, Júlia Eduarda",
+    descricao: ".",
+    turma: "2ºB"
+  },
+  {
+    id: 19,
+    nome: "Episódio 19",
+    audioSrc: "/",
+    capitulos: "101, 102, 103, 104, 105, 106",
+    alunos: "Rafaela, Matheus Bassi, Max",
+    descricao: ".",
+    turma: "2ºA"
+  },
+  {
+    id: 20,
+    nome: "Episódio 20",
+    audioSrc: "/",
+    capitulos: "107, 108, 109, 110, 111, 112",
+    alunos: "Eduardo Fernandes, Mateus Orocondo, Yan Fernando",
+    descricao: ".",
+    turma: "2ºB"
+  },
+  {
+    id: 21,
+    nome: "Episódio 21",
+    audioSrc: "/",
+    capitulos: "113, 114, 115, 116, 117, 118",
+    alunos: "Emilly Lemes, Dalila",
+    descricao: ".",
+    turma: "2ºA"
+  },
+  {
+    id: 22,
+    nome: "Episódio 22",
+    audioSrc: "/",
+    capitulos: "119, 120, 121, 122, 123, 124",
+    alunos: "Pedro Noemerg, Aline Noemerg",
+    descricao: ".",
+    turma: "2ºB"
+  },
+  {
+    id: 23,
+    nome: "Episódio 23",
+    audioSrc: "/",
+    capitulos: "125, 126, 127, 128, 129, 130",
+    alunos: "Dayane, Alana",
+    descricao: ".",
+    turma: "2ºA"
+  },
+  {
+    id: 24,
+    nome: "Episódio 24",
+    audioSrc: "/",
+    capitulos: "131, 132, 133, 134, 135, 136",
+    alunos: "Emanuel, Henrique",
+    descricao: ".",
+    turma: "2ºA"
+  },
+  {
+    id: 25,
+    nome: "Episódio 25",
+    audioSrc: "/",
+    capitulos: "137, 138, 139, 140, 141, 142",
+    alunos: "Gabriely, Tamires",
+    descricao: ".",
+    turma: "2ºA"
+  },
+  {
+    id: 26,
+    nome: "Episódio 26",
+    audioSrc: "/",
+    capitulos: "143, 144, 145, 146, 147, 148",
+    alunos: "Kauã, Leonardo Neiva",
+    descricao: ".",
     turma: "2ºA"
   },
 ];
 
-export default episodios;
+export default Episódios;
