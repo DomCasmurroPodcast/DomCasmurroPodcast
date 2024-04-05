04cf10dfe514741ea9a206790cd8876e9f168a5a
João Vitor Pereira <JVPG789@hotmail.com>
2024-04-05 13:48:10 -0400

    Adição dos episodios

--- src/pages/Episodio/[id].jsx
+++ src/pages/Episodio/[id].jsx
@@ -1,24 +1,18 @@
 import { useRouter } from "next/router";
 import episodios from "@/utils/episodios";
-import returnSrc from "@/utils/returnSrc";
 import styles from "@/styles/Episodio.module.scss";
 import { useState, useEffect } from "react";
 import EpisodioButton from "@/components/EpisodioButton";
 import Button from "@/components/Button";
 import Link from "next/link";
 
-// export async function getServerSideProps(context) {
-//   return {
-//     props: {},
-//   };
-// }
 
 export default function Page() {
   const router = useRouter();
   const episodio = episodios[router.query.id - 1];
   const episodioAnterior = episodios[router.query.id - 2];
   const episodioProximo = episodios[router.query.id];
-  const src = returnSrc( episodio?.audioSrc ? episodio.audioSrc : null);
+  const src = "/Audio/Episodes/" + episodio?.audioSrc;
   const [seed, setSeed] = useState(1);
   const [windowWidth, setWindowWidth] = useState();
   const [podeCarregar, setPodeCarregar] = useState(false);
